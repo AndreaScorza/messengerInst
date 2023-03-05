8fce9d2c4b24fa68321194af56638d6822f2130a
AndreaScorza <atlantas95@gmail.com>
2023-03-05 19:27:04 +0100

    removed data file, updated main with link

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import rw_to_file as rw
 
 # instantiate the file
 file = rw.StringArrayFile('data.txt')
+link = 'https://www.instagram.com/direct/inbox/?hl=en'
 
 #retrieve messages from instagram
 messages = inst_module.getMessages()
 
-# messages = [['PersonA', 'Test']]
+# uncomment line below and comment line above to test message on telegram
+# messages = [['PersonA', 'Text']]
 
 for message in messages:
     if (not file.contains(message[0] + message[1])):
         telegram_mess.send_message(message[0] + '\n' + message[1])
         file.append(message[0] + message[1])
+    telegram_mess.send_message(link)
